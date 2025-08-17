116fb7fd5a88f925e4d220855b8b12d7547fdf21
Marco Lucio <marcoalucio15@gmail.com>
2025-08-16 22:14:35 -0600

    fixed types on custom cursor

--- app/components/ui/CustomCursor.tsx
+++ app/components/ui/CustomCursor.tsx
@@ -1,40 +1,48 @@
 // components/CustomCursor.js
 "use client";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [cursorState, setCursorState] = useState('');
+  const [cursorState, setCursorState] = useState("");
 
   useEffect(() => {
-    const updatePosition = (e: any) => {
+    const updatePosition = (e: MouseEvent) => {
       setPosition({ x: e.clientX, y: e.clientY });
-      
-      const target = e.target;
-      
+
+      const target = e.target as HTMLElement;
+
       // some manual checking to trigger changes in styling
-      if (target.matches('a, a *')) {
-        setCursorState('link'); // don't think i need this one
-      } else if (target.matches('button, [role="button"], input[type="submit"], input[type="button"], .clickable, button *, image *')) {
-        setCursorState('hover');
-      } else if (target.matches('input, textarea, [contenteditable], input *, textarea *')) {
-        setCursorState('text');
+      if (target.matches("a, a *")) {
+        setCursorState("link"); // don't think i need this one
+      } else if (
+        target.matches(
+          'button, [role="button"], input[type="submit"], input[type="button"], .clickable, button *, image *'
+        )
+      ) {
+        setCursorState("hover");
+      } else if (
+        target.matches(
+          "input, textarea, [contenteditable], input *, textarea *"
+        )
+      ) {
+        setCursorState("text");
       } else {
-        setCursorState('');
+        setCursorState("");
       }
     };
 
-    document.addEventListener('mousemove', updatePosition);
-    return () => document.removeEventListener('mousemove', updatePosition);
+    document.addEventListener("mousemove", updatePosition);
+    return () => document.removeEventListener("mousemove", updatePosition);
   }, []);
 
   return (
     <div
       className={`custom-cursor ${cursorState}`}
-      style={{ 
-        left: `${position.x}px`, 
-        top: `${position.y}px` 
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
       }}
     />
   );
