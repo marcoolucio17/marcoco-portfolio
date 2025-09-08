ef9d206e729b4cd698e72474ccaaf7f406a10274
Marco Lucio <marcoalucio15@gmail.com>
2025-09-07 21:40:52 -0600

    updated about-me section

--- src/app/components/sections/AboutMe.tsx
+++ src/app/components/sections/AboutMe.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { FlippingCard } from "../ui/FlippingCard";
 import { Typewriter } from "nextjs-simple-typewriter";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import { ContactLink } from "../ui/ContactLink";
 
 export default function AboutMe() {
   const tAbout = useTranslations("about");
@@ -15,45 +17,91 @@ export default function AboutMe() {
     tIntro("typewriterWords.2"),
     tIntro("typewriterWords.3"),
     tIntro("typewriterWords.4"),
-  ]
+  ];
 
   return (
     <div id="about-me-main-div" className="flex flex-col w-full h-full gap-20">
-      <div>
-        <h1>{tAbout("title")}</h1>
-      </div>
-
       <div
         id="image-and-desc-div"
-        className="flex flex-col lg:flex-row w-full gap-6 lg:gap-20"
+        className="flex flex-col lg:flex-row w-full h-full gap-10 lg:gap-20 lg:justify-between"
       >
-        <div className="">
-          <FlippingCard
-            imageSrc="/marcocoysol.jpg"
-            width={300}
-            height={300}
-            imageAlt={tAbout("flipcard.alt")}
-            backText={tAbout("flipcard.backText")}
-          />
+        <div className="w-full lg:w-1/3 flex flex-col justify-start min-h-[400px] lg:min-h-[500px]">
+          {/* Header at top */}
+          <div className="font-sans text-xl lg:text-2xl">
+            <h2 className="font-sans leading-relaxed">
+              I'm Marco Lucio, {" "}
+              <span className="text-gray-400 ml-1">
+                a Software Engineer Intern at Oracle.
+              </span>
+            </h2>
+          </div>
+
+          <div className="mt-20">
+            <h3 className="font-sans text-sm lg:text-base leading-relaxed">
+              {tAbout("description")}
+            </h3>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-12 md:gap-7 flex-1">
-          <h2 className="h-2/3 md:h-1/3 lg:h-1/3">
-            {tIntro("greeting")}
-            <Typewriter
-              words={writerMessages}
-              loop={0}
-              cursor
-              cursorStyle="_"
-              typeSpeed={70}
-              deleteSpeed={50}
-              delaySpeed={1000}
+        {/* Right side - Image and Social Media */}
+        <div className="w-full lg:w-1/2 flex flex-col justify-between min-h-[400px] lg:min-h-[500px]">
+          {/* Image at top */}
+          <div className="flex justify-center lg:justify-end">
+            <Image
+              src="/marcosombrero.png"
+              alt="marco w/ a hat"
+              height={200}
+              width={600}
+              className="lg:h-[300px] lg:w-[600px] object-cover rounded-2xl"
             />
-          </h2>
+          </div>
 
-          <p className="font-mono">
-            {tAbout("description")}
-          </p>
+          {/* Social media at bottom */}
+          <div
+            id="social-media-main-container"
+            className="flex flex-col gap-8 lg:gap-10 mt-10"
+          >
+            <div
+              id="social-media-row-1"
+              className="flex flex-row justify-center lg:justify-start gap-12 lg:gap-20"
+            >
+              {/* <h3 className="font-mono text-sm lg:text-base tracking-wider">SPOTIFY</h3> */}
+              <ContactLink
+                type="spotify"
+                address="SPOTIFY"
+                url="https://open.spotify.com/user/luigilucs"
+              />
+
+              <ContactLink
+                type="steam"
+                address="STEAM"
+                url="https://steamcommunity.com/profiles/76561199251618925/"
+              />
+
+            </div>
+
+            <div
+              id="social-media-row-2"
+              className="flex flex-row justify-center lg:justify-end"
+            >
+              <ContactLink
+                type="github"
+                address="github.com/marcoolucio17"
+                url="https://github.com/marcoolucio17"
+              />
+            </div>
+
+            <div
+              id="social-media-row-3"
+              className="flex flex-row justify-center lg:justify-start"
+            >
+              <ContactLink
+                type="linkedin"
+                address="Marco Lucio"
+                url="https://www.linkedin.com/in/marco-lucio-3a24b0214/"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
